8e107d2db22a34b099a58e2b8e96f3dafe795b75
Diego Bonilla <diego.bonilla@jalasoft.com>
2024-02-13 22:59:24 -0500

    Step 3: Start Scene

--- js/Start.js
+++ js/Start.js
@@ -4,8 +4,21 @@ class Start extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('alienBg', './assets/img/aliens_screen_image.jpg');
+        this.load.image ('startButton', './assets/img/start.png');
     }
 
     create() {
+        this.alienBg = this.add.image(0,0,'alienBg');
+        this.alienBg.setOrigin(0,0);
+        this.alienBg.setScale(game.canvas.width / this.alienBg.width, game.canvas.height / this.alienBg.height);
+        this.startText = this.add.text(game.canvas.width / 2, game.canvas.height / 2, 'Space Aliens', {font: '50px Arial', fill: '#fff'}).setOrigin(0.5) 
+
+        this.startButton = this.add.image(game.canvas.width / 2, (game.canvas.height / 2) + 150, 'startButton').setScale(0.7);
+        this.startButton.setInteractive({useHandCursor: true});
+        
+        this.startButton.on('pointerdown', () => {
+            this.scene.switch('Game');
+        });
     }
 }
